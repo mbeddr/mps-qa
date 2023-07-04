71192bab2a8c8977917543de1b423fb6ba45771f
Sergej Koščejev <sergej@koscejev.cz>
2023-07-04 12:55:20 +0200

    build: simplify the build script

---
+++ settings.gradle.kts
@@ -0,0 +1,12 @@
+pluginManagement {
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/mbeddr/*")
+            credentials {
+                username = if (extra.has("github_username")) extra.get("github_username") as String else System.getenv("GITHUB_ACTOR")
+                password = if (extra.has("github_token")) extra.get("github_token") as String else System.getenv("GITHUB_TOKEN")
+            }
+        }
+        gradlePluginPortal()
+    }
+}
