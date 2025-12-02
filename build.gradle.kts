49cffcb4d87372c5a0e7541c948361dec4b174f9
MB Takim <146926533+btakim@users.noreply.github.com>
2025-12-02 13:30:29 +0100

    Update outdated dependencies (#401)
    * Update outdated dependencies
    
    org.apache.commons:commons-lang3
    commons-io:commons-io
    
    * Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,9 +65,9 @@ dependencies {
 
     plantuml("net.sourceforge.plantuml:plantuml-asl:1.2023.13")
 
-    baseLib("org.apache.commons:commons-lang3:3.3.2")
+    baseLib("org.apache.commons:commons-lang3:3.20.0")
     baseLib("commons-cli:commons-cli:1.5.0")
-    baseLib("commons-io:commons-io:2.6")
+    baseLib("commons-io:commons-io:2.21.0")
 
     treemap("net.sf.jtreemap:jtreemap:1.1.3")
     treemap("net.sf.jtreemap:ktreemap:1.1.0-atlassian-01")
